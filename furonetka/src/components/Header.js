b83f990ec80f9fc04f364b8e9adf125061c97075
Dawid <ossowski.dawid@gmail.com>
2022-01-22 19:54:25 +0100

    wip

--- furonetka/src/components/Header.js
+++ furonetka/src/components/Header.js
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../index.css";
 
 export const Header = () => {
   const initialValues = { email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (event) => {
-    console.log(event.target);
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[%\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = "To pole nie moze być puste! Proszę podać adres e-mail";
+    }
+    if (!values.password) {
+      errors.password = "To pole nie moze być puste! Proszę podać hasło";
+    }
+    return errors;
   };
 
   return (
@@ -24,34 +52,45 @@ export const Header = () => {
             <p style={{ color: "#228cc5" }}>
               Udostępnij najlepszą ofertę na rynku i dodatkowo zarabiaj
             </p>
-            <form className="form">
+            {/* <pre>{JSON.stringify(formValues, undefined, 2)}</pre> */}
+          </div>
+          <div className="form">
+            <div className="form__content">
               <p>Zacznij zarabiać w 5 minut</p>
               <p style={{ fontSize: "12px" }}>
                 Zarejestruj się w minutę i sprawdź, jak to działa
               </p>
-              <input
-                type="text"
-                name="email"
-                placeholder="jan.kowalski@gmail.com"
-                value={formValues.email}
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="password"
-                name="password"
-                placeholder="Hasło"
-                value={formValues.password}
-                onChange={handleChange}
-                required
-              />
-              <p style={{ color: "#919191", fontSize: "9px" }}>
-                Uzyj od 6 do 24 znaków. Nie uzywaj łatwych sekwencji, np.
-                "123456"
-              </p>
-              <button type="submit" className="btn__submit">
-                Załóz darmowe konto
-              </button>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="form__box">
+                <div className="form__inputs">
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="jan.kowalski@gmail.com"
+                    value={formValues.email}
+                    onChange={handleChange}
+                  />
+                  <p className="form__error">{formErrors.email}</p>
+                </div>
+                <div className="form__inputs">
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Hasło"
+                    value={formValues.password}
+                    onChange={handleChange}
+                  />
+                  <p style={{ color: "#919191", fontSize: "9px" }}>
+                    Uzyj od 6 do 24 znaków. Nie uzywaj łatwych sekwencji, np.
+                    "123456"
+                  </p>
+                  <p className="form__error">{formErrors.password}</p>
+                </div>
+                <button type="submit" className="btn__submit">
+                  Załóz darmowe konto
+                </button>
+              </div>
             </form>
           </div>
         </div>
