d7bde399aa68abdcfe8240b73f71c0de9ab8eff9
Dawid <ossowski.dawid@gmail.com>
2022-01-23 19:09:32 +0100

    task done

--- furonetka/src/components/MobileSize.js
+++
@@ -1,5 +0,0 @@
-// @media only screen and (min-width: 1200px){}
-// @media only screen and (min-width: 992px) and (max-width: 1199px) {}
-// @media only screen and (min-width: 768px) and (max-width: 991px){}
-// @media only screen and (min-width: 481px) and(max-width: 767px) { }
-// @media only screen and (max-width: 480px) { }
